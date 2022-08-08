1df3557e16e8c30baec3128628675782c4724047
Tristan Delforge <tristandelforge1@gmail.com>
2022-08-08 09:29:03 -0700

    player win predictor started

--- scrapers/tracker_gg_valorant_scraper.py
+++ scrapers/tracker_gg_valorant_scraper.py
@@ -11,7 +11,7 @@ import glob
 import os
 import random
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -37,9 +37,10 @@ def scrape_match(match_id):
 
 
 def get_match_ids_from_userpage(user_id):
+    logger.info(f'Getting matches for user_id: {user_id}')
     driver = get_driver()
     driver.get(f'https://tracker.gg/valorant/profile/riot/{user_id.replace("#", "%23")}/overview')
-    time.sleep(5)
+    time.sleep(10)
     soup = BeautifulSoup(driver.page_source)
     driver.close()
     links = soup.find_all('a', href=True)
@@ -48,18 +49,27 @@ def get_match_ids_from_userpage(user_id):
 
 
 def get_user_ids_from_match_data(match_id):
-    with open(f'{Values.valorant_data_loc}/{match_id}.json', 'r') as f:
-        json_data = json.load(f)
+    try:
+        with open(f'{Values.valorant_data_loc}/{match_id}.json', 'r') as f:
+            json_data = json.load(f)
 
-    json_data_loaded = json.loads(json_data)
+        json_data_loaded = json.loads(json_data)
 
-    users = list()
+        users = list()
 
-    for i in json_data_loaded['data']['segments']:
-        if 'attributes' in i and 'platformUserIdentifier' in i['attributes']:
-            users.append(i['attributes']['platformUserIdentifier'])
+        if 'data' not in json_data:
+            print(f"error {f'{Values.valorant_data_loc}/{match_id}.json'}")
+            return list()
+        else:
 
-    return list(set(users))
+            for i in json_data_loaded['data']['segments']:
+                if 'attributes' in i and 'platformUserIdentifier' in i['attributes']:
+                    users.append(i['attributes']['platformUserIdentifier'])
+
+            return list(set(users))
+    except:
+        logging.warning(f'error reading match id: {match_id}')
+        return list()
 
 
 def get_all_match_ids():
@@ -67,20 +77,32 @@ def get_all_match_ids():
     return [os.path.basename(i).split('.')[0] for i in files]
 
 
+def main():
+    num_of_iterations=5
+
+    for iteration in range(num_of_iterations):
+        try:
+            match_ids = get_all_match_ids()
+            user_ids = list()
+            for i in match_ids:
+                user_ids.extend(get_user_ids_from_match_data(i))
+            user_ids = list(set(user_ids))
+            random.shuffle(user_ids)
+
+            for user_id in user_ids:
+                time.sleep(10)
+                match_ids = get_match_ids_from_userpage(user_id)
+
+                logger.info(f'User: {user_id}, match_count: {len(match_ids)}' )
+
+                for i in match_ids:
+                    time.sleep(10)
+                    scrape_match(i)
+        except Exception as e:
+            logger.exception(e)
+
+            time.sleep(900)
+
+
 if __name__ == '__main__':
-
-    match_ids = get_all_match_ids()
-    user_ids = list()
-    for i in match_ids:
-        user_ids.extend(get_user_ids_from_match_data(i))
-    user_ids = list(set(user_ids))
-    random.shuffle(user_ids)
-
-    for user_id in user_ids:
-        match_ids = get_match_ids_from_userpage(user_id)
-
-        logger.info(f'User: {user_id}, match_count: {len(match_ids)}' )
-
-        for i in match_ids:
-            time.sleep(10)
-            scrape_match(i)
+    main()
